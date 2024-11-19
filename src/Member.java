250acb6c0664ec7a0611b61b32d142ddc078fa1f
Harning Home <hejthenerd@gmail.com>
2024-11-19 16:27:53 +0100

    Added a constructer for Member

--- src/Member.java
+++ src/Member.java
@@ -1,4 +1,17 @@
 public class Member {
     int memberID;
     String memberName;
+    int memberAge;
+    String memberPhoneNumber;
+    boolean isActiveMember;
+    boolean isCompeting;
+
+    Member(int ID, String name, int age, String phoneNum, String active, String competing){
+        memberID=ID;
+        memberName=name;
+        memberPhoneNumber=phoneNum;
+        memberAge=age;
+        isActiveMember = active.equalsIgnoreCase("active");
+        isCompeting = competing.equalsIgnoreCase("competing");
+    }
 }
