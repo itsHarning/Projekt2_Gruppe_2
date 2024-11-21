a28028a4f30be7d452dde965b8b78608137d8e87
Harning Laptop <hejthenerd@gmail.com>
2024-11-21 11:08:50 +0100

    changed memberID to follow naming convention , is now memberId

--- src/Member.java
+++ src/Member.java
@@ -1,16 +1,16 @@
 public class Member {
-    int memberID;
+    int memberId;
     String memberName;
     int memberAge;
     String memberPhoneNumber;
     boolean isActiveMember;
     boolean isCompeting;
 
-    Member(int ID, String name, int age, String phoneNum, String active, String competing){
-        memberID=ID;
-        memberName=name;
-        memberPhoneNumber=phoneNum;
-        memberAge=age;
+    Member(int Id, String name, int age, String phoneNum, String active, String competing){
+        memberId = Id;
+        memberName = name;
+        memberPhoneNumber = phoneNum;
+        memberAge = age;
         isActiveMember = active.equalsIgnoreCase("active");
         isCompeting = competing.equalsIgnoreCase("competing");
     }
