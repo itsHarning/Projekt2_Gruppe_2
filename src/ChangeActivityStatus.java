a556f955c9c4b34d06e331f10140f6d2f1b6dab2
guneyozcan <guoz0001@stud.kea.dk>
2024-11-25 15:57:15 +0100

    Har lavet en change activity status.rettet men ikke færdig endnu

--- src/ChangeActivityStatus.java
+++ src/ChangeActivityStatus.java
@@ -1,10 +1,24 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ChangeActivityStatus {
     public static void main(String[] args) {
 
+        ArrayList<Member> testlist = new ArrayList<>();
+        Member a1 = new Member(1, "j1", 16, true, true, false, false);
+        Member a2 = new Member(2, "j2", 20, false, true, false, false);
+        Member a3 = new Member(3, "j3", 61, true, true, false, false);
+        Member a4 = new Member(4, "j4", 18, true, true, false, false);
+        Member a5 = new Member(5, "j5", 18, true, true, false, false);
 
-        Member member = new Member(1, "Test", 18, true, true, true);
+        testlist.add(a1);
+        testlist.add(a2);
+        testlist.add(a3);
+        testlist.add(a4);
+        testlist.add(a5);
+
+
+        Member member = new Member(1, "Test", 18, true, true, true,true);
         System.out.println(member.isActiveMember);
         member=changeActivityStatus(member);
         System.out.println(member.isActiveMember);
