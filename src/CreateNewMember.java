fc5732fbda895d4a357a6942e47b7a2106f561d3
Joadoa1992 <jolu0001@stud.kea.dk>
2024-11-26 13:56:45 +0100

    I have made a menu in main and made CreateNewMember a littel better

--- src/CreateNewMember.java
+++ src/CreateNewMember.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CreateNewMember {
     static Scanner keyboard = new Scanner(System.in);
-
+    static ArrayList<Member> membersList;
     public static Member createNewMember() {
+        membersList= MemberHandler.loadMembersFromTextFile();
+
         System.out.println("Hvad er dit navn?");
         String memberName = keyboard.nextLine();
         if (memberName.equals("0")) System.exit(0);
@@ -13,39 +16,58 @@ public class CreateNewMember {
         int memberAge = getIntFromUser(keyboard);
         if (memberAge == 0) System.exit(0);
 
-        boolean isActiveMember = true;
+        System.out.println("Er medlemmet aktiv eller passiv?");
+        String answerStatus = keyboard.nextLine();
+        boolean isActiveMember;
+        while (true) {
+            if (answerStatus.equalsIgnoreCase("aktiv")) {
+                isActiveMember = true;
+                break;
+            } else if (answerStatus.equalsIgnoreCase("passiv")) {
+                isActiveMember = false;
+                break;
+            } else {
+                System.out.println("Det er ikke et gyldigt svar, prøv igen (aktivt/passivt)");
+                answerStatus = keyboard.nextLine();
+            }
+        }
         System.out.println("Vil du stille op i stævner? ja eller nej");
-        boolean isCompeting = false;
-        String answer = keyboard.nextLine();
-        if (answer.equals("0")) System.exit(0);
+        boolean isCompeting;
+
+        String answerCompeting = keyboard.nextLine();
+        if (answerCompeting.equals("0")) System.exit(0);
 
         while (true) {
-            if (answer.equalsIgnoreCase("ja")) {
+            if (answerCompeting.equalsIgnoreCase("ja")) {
                 isCompeting = true;
                 break;
-            } else if (answer.equalsIgnoreCase("nej")) {
+            } else if (answerCompeting.equalsIgnoreCase("nej")) {
                 isCompeting = false;
                 break;
             } else {
                 System.out.println("Det er ikke et gyldigt svar, prøv igen (ja/nej)");
-                answer = keyboard.nextLine();
+                answerCompeting = keyboard.nextLine();
             }
         }
         boolean hasPaid = false;
         boolean automatikPaid = false;
-        System.out.println("Du har skrevet, navn: " + memberName + ". Alder: " + memberAge + ". Hans kompetetiv status er: " + isCompeting);
+        System.out.println("Du har skrevet, navn: " + memberName + ". Alder: " + memberAge +" . Hans status: "+isActiveMember+ ". Hans kompetetiv status er: " + isCompeting);
+        membersList.add(new Member(memberName, memberAge, isActiveMember, isCompeting, hasPaid, automatikPaid));
+        MemberHandler.updateTextFile(membersList);
         return new Member(memberName, memberAge, isActiveMember, isCompeting, hasPaid, automatikPaid);
+        // TODO: Member kommer ind i member list
+
     }
 
     public static int getIntFromUser(Scanner keyboard) {
         int result = 0;
         boolean validInput = false;
 
-        // Fortsæt indtil vi får et gyldigt input
+        // Continue until you get a valid input
         while (!validInput) {
             try {
-                result = Integer.parseInt(keyboard.nextLine()); // Læs input som en int
-                validInput = true; // Hvis input er gyldigt, afslut løkken
+                result = Integer.parseInt(keyboard.nextLine()); // Read input as an int
+                validInput = true; // If input is invalid, it ends the loop
             } catch (NumberFormatException e) {
                 System.out.println("Ugyldigt input! Indtast venligst et helt tal.");
             }
