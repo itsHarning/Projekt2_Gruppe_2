ada26dee0af98c9275e104bf7a9c1b578a469019
Harning Laptop <hejthenerd@gmail.com>
2024-11-27 15:57:27 +0100

    Minor change

--- src/GetTopFiveSwimmers.java
+++ src/GetTopFiveSwimmers.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+
 public class GetTopFiveSwimmers {
 
+    GetTopFiveSwimmers(ArrayList<Member> memberList, ArrayList<Double> discipline){
+        ArrayList<Member> tempList;
+        for (Member m : memberList){
 
-    GetTopFiveSwimmers(){
-
+        }
     }
 }
