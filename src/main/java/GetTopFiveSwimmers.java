0dfa89eeed364f21927c8b71f9ab925ce5e56ffb
Harning Home <hejthenerd@gmail.com>
2024-12-03 19:22:07 +0100

    added Maven to the project to be able to work with json

--- src/main/java/GetTopFiveSwimmers.java
+++ src/main/java/GetTopFiveSwimmers.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class GetTopFiveSwimmers {
-
+    
     GetTopFiveSwimmers(ArrayList<Member> memberList, ArrayList<Double> discipline){
         ArrayList<Member> tempList;
         for (Member m : memberList){
