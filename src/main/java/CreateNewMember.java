184fb468ce6632583f6b45be6b5813547d3104b3
Joadoa1992 <jolu0001@stud.kea.dk>
2024-12-09 12:19:03 +0100

    I have fixed CreateNewMember

--- src/main/java/CreateNewMember.java
+++ src/main/java/CreateNewMember.java
@@ -4,12 +4,16 @@ import java.util.Scanner;
 public class CreateNewMember {
     static Scanner keyboard = new Scanner(System.in);
 
+
+    // In this method you can make a new member witch is then set in memberList, json and creates a compobject for them
     public static void createNewMember(ArrayList<Member> membersList) {
 
-                System.out.println("Hvad er medlemmets navn?");
+        // Here you can input name as a string
+        System.out.println("Hvad er medlemmets navn?");
         String Name = keyboard.nextLine();
-        if (Name.equals("0")) System.exit(0);
+        if (Name.equals("0")) return;                   // if you press 0 you get send back to the menu
 
+        // here you enter the gender
         System.out.println("Hvilen køn er medlemmet? (Mand/Kvinde/Andet)");
 
         String Valg = keyboard.nextLine();
@@ -33,13 +37,21 @@ public class CreateNewMember {
             }
         }
 
+        // here you enter you're age
         System.out.println("Hvor gammel er medlemmet?");
         int Age = getIntFromUser(keyboard);
-        if (Age == 0) System.exit(0);
+        // tjeks if the age is over 100
+        while(Age >= 100){
+            System.out.println("Du skal melde dig på pensionist swømmeklubben hvis du er over 100 (Du kan gå tilbage ved at taste 0)");
+            Age = getIntFromUser(keyboard);
+        }
+        if (Age == 0) return;
 
+        // Ask if the member is aktive or not
         System.out.println("Er medlemskabet aktivt? (Ja / Nej)");
         String answerStatus = keyboard.nextLine();
         boolean isActiveMember;
+        if (answerStatus.equalsIgnoreCase("0")) return;
         while (true) {
             if (answerStatus.equalsIgnoreCase("ja")) {
                 isActiveMember = true;
@@ -52,34 +64,44 @@ public class CreateNewMember {
                 answerStatus = keyboard.nextLine();
             }
         }
-        System.out.println("Vil medlemmet stille op i stævner? (Ja / Nej)");
+
+        // Ask if the member is competing
         boolean isCompeting;
+        if(isActiveMember) {                                                        // tjeks if the member is aktive and if not goes to the next kode
+            System.out.println("Vil medlemmet stille op i stævner? (Ja / Nej)");
 
-        String answerCompeting = keyboard.nextLine();
-        if (answerCompeting.equals("0")) System.exit(0);
+            String answerCompeting = keyboard.nextLine();
+            if (answerCompeting.equals("0")) return;
 
-        while (true) {
-            if (answerCompeting.equalsIgnoreCase("ja")) {
-                isCompeting = true;
-                break;
-            } else if (answerCompeting.equalsIgnoreCase("nej")) {
-                isCompeting = false;
-                break;
-            } else {
-                System.out.println("Det er ikke et gyldigt svar, prøv igen (Ja / Nej)");
-                answerCompeting = keyboard.nextLine();
+            while (true) {
+                if (answerCompeting.equalsIgnoreCase("ja")) {
+                    isCompeting = true;
+                    break;
+                } else if (answerCompeting.equalsIgnoreCase("nej")) {
+                    isCompeting = false;
+                    break;
+                } else {
+                    System.out.println("Det er ikke et gyldigt svar, prøv igen (Ja / Nej)");
+                    answerCompeting = keyboard.nextLine();
+                }
             }
+        }
+        else {
+            isCompeting = false;
         }
         boolean hasPaid = false;
         boolean automaticPayment = false;
+        // gives message of what you have ridden
         System.out.println("Du har skrevet, navn: "+Name+", Køn: "+gender+", Alder: "+Age+", Medlemskabs status status: "+isActiveMember+". Kompetetiv status: "+isCompeting);
 
+        // here it adds the information to memberList, jsonfile and creates a new object
         membersList.add(new Member(Name, gender,  Age, isActiveMember, isCompeting, hasPaid, automaticPayment));
         // Team.assignTeams(membersList);
         CreateCompObject.createCompObject(membersList);
         FileHandler.writeListToJson(membersList);
     }
 
+    // this method tjeks if the int is valid
     public static int getIntFromUser(Scanner keyboard) {
         int result = 0;
         boolean validInput = false;
